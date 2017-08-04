0c34b1ec2cfbe86c0e02e8e01702c4af316fdbc3
yuta1331 <yua.yoyoyo@gmail.com>
2017-08-04 20:05:36 +0900

    add function: write out data_list and watermark to files

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ BITSIZE = 32
 
 ###### parse ######
 data_list = html_parser('watermarked.txt')
+with open('data_list', 'w') as f:
+  for data in data_list:
+    f.write(str(data) + '\n')
 print('========== data_list ==========')
 print(data_list)
 
 ###### detect ######
 detected_watermark = detector(data_list, BITSIZE)
+with open('watermark', 'w') as f:
+  if type(detected_watermark) != str:
+    print('ERROR: detected_watermark is not type of str')
+  else:
+    f.write(detected_watermark)
 print('========== detected_watermark ==========')
 print(detected_watermark)
